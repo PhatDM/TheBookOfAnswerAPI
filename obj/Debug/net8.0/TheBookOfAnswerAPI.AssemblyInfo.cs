591ff319d4c29445cc120dff744ab925cdd4cbfe
PhatDM <dominhphat97@gmail.com>
2024-04-13 10:43:15 +0700

    5
    5

--- obj/Debug/net8.0/TheBookOfAnswerAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/TheBookOfAnswerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheBookOfAnswerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e2e48c93340ce3edbd1781e912a53877deb12fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2878f62c8a82a88d83cb80d77c53e416376e818c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheBookOfAnswerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheBookOfAnswerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
